eeaebe66504075f6a1df1068338ca68ba7188aa0
Hilal Güvenç <hilalguvenc@yahoo.com>
2020-05-14 15:46:55 +0300

    sorted union

---
+++ sorted_union.js
@@ -0,0 +1,12 @@
+function uniteUnique(...arr) {
+  var newArr = [];
+  for (x = 0; x < arr.length; x++) {
+    for (y = 0; y < arr[x].length; y++) {
+      if (!newArr.includes(arr[x][y])) {
+        newArr.push(arr[x][y]);
+      }
+    }
+  }
+  return newArr;
+}
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
